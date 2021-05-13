9a2c0eeb29a5afdac5b257c23884e14304bea670
cpach96 <cpach96@gmail.com>
2021-05-13 11:18:36 -0400

    commit tree ca53404b1b0380fc86fed64a7716ef1d085e1598

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
-// Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+export default class CoordinatesButton extends Component {
+
+    mouseCoordinates = (event) => {
+        let newCoords = [event.clientX,event.clientY]
+        return this.props.onReceiveCoordinates(newCoords)
+    }
+
+    render(){
+        return(
+            <button onClick={this.mouseCoordinates}/>
+        )
+    }
+
+}
+
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+export default class DelayedButton extends Component {
+
+    eventHandler = (event) =>{
+        event.persist();
+        let tester = this.props.onDelayedClick
+        setTimeout(function(){ tester(event) }, this.props.delay);
+    }
+
+    render(){
+        return(
+            <button onClick={this.eventHandler}/>
+    
+        )
+    }
+
+}
+
